c3bd64d42a5cca741120906c211dd56a7f0964c6
Kyle McDaniel <mcdanieljk8@gmail.com>
2017-07-16 07:28:33 +0000

    update dburl

--- app.js
+++ app.js
@@ -15,10 +15,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://mcdanieljk8:KissMyAss55@ds161262.mlab.com:61262/yelp_camp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://mcdanieljk8:KissMyAss55@ds161262.mlab.com:61262/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
